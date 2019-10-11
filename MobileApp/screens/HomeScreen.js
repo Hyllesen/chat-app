008980e6f8917f1e7de32928f485a632c322a733
Hyllesen <stefanhyltoft@gmail.com>
2019-10-11 16:42:43 +0200

    Now sending full message object from socket.io

--- MobileApp/screens/HomeScreen.js
+++ MobileApp/screens/HomeScreen.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState, useRef } from "react";
-import { StyleSheet, View, Platform, KeyboardAvoidingView } from "react-native";
+import { View, Platform, KeyboardAvoidingView } from "react-native";
 import io from "socket.io-client";
 import { GiftedChat } from "react-native-gifted-chat";
 
 export default function HomeScreen() {
-  const [messageToSend, setMessageToSend] = useState("");
   const [recvMessages, setRecvMessages] = useState([]);
   const socket = useRef(null);
 
   useEffect(() => {
     socket.current = io("http://192.168.0.10:3001");
     socket.current.on("message", message => {
+      debugger;
+      console.log("message", message);
       setRecvMessages(prevState => GiftedChat.append(prevState, message));
     });
   }, []);
@@ -18,6 +19,7 @@ export default function HomeScreen() {
   const onSend = messages => {
     console.log(messages);
     socket.current.emit("message", messages[0].text);
+    setRecvMessages(prevState => GiftedChat.append(prevState, messages));
   };
 
   return (
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,22 +1,31 @@
 const io = require("socket.io")();
 
-// const testMessage = {
-//   _id: 3,
-//   text: "Hello developer",
-//   createdAt: new Date(),
-//   user: {
-//     _id: 2,
-//     name: "React Native",
-//     avatar: "https://placeimg.com/140/140/any"
-//   }
-// };
+let currentUserId = 2;
+let currentMessageId = 1;
+const userIds = {};
+
+function createMessage(userId, messageText) {
+  return {
+    _id: currentMessageId++,
+    text: messageText,
+    createdAt: new Date(),
+    user: {
+      _id: userId,
+      name: "Test User",
+      avatar: "https://placeimg.com/140/140/any"
+    }
+  };
+}
 
 io.on("connection", socket => {
   console.log("a user connected!");
   console.log(socket.id);
-  socket.on("message", message => {
+  userIds[socket.id] = currentUserId++;
+  socket.on("message", messageText => {
+    const userId = userIds[socket.id];
+    const message = createMessage(userId, messageText);
     console.log(message);
-    io.emit("message", message);
+    socket.broadcast.emit("message", message);
   });
 });
 
