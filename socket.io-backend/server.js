1d8bf5f46fdb0533fb8401961e77ac29f22c1789
Hyllesen <stefanhyltoft@gmail.com>
2019-10-14 07:21:12 +0200

    Created join event handler

--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -2,13 +2,16 @@ const io = require("socket.io")();
 const messageHandler = require("./handlers/message.handler");
 
 let currentUserId = 2;
-const userIds = {};
+const users = {};
 
 io.on("connection", socket => {
   console.log("a user connected!");
   console.log(socket.id);
-  userIds[socket.id] = currentUserId++;
-  messageHandler.handleMessage(socket, userIds);
+  users[socket.id] = { userId: currentUserId++ };
+  socket.on("join", username => {
+    users[socket.id].username = username;
+  });
+  messageHandler.handleMessage(socket, users);
 });
 
 io.listen(3001);
