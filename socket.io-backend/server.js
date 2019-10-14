42962e1fa8a0d49300840c52f95e2b01b423c5d0
Hyllesen <stefanhyltoft@gmail.com>
2019-10-14 06:09:29 +0200

    Created message handler

--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -1,32 +1,14 @@
 const io = require("socket.io")();
+const messageHandler = require("./handlers/message.handler");
 
 let currentUserId = 2;
-let currentMessageId = 1;
 const userIds = {};
-
-function createMessage(userId, messageText) {
-  return {
-    _id: currentMessageId++,
-    text: messageText,
-    createdAt: new Date(),
-    user: {
-      _id: userId,
-      name: "Test User",
-      avatar: "https://placeimg.com/140/140/any"
-    }
-  };
-}
 
 io.on("connection", socket => {
   console.log("a user connected!");
   console.log(socket.id);
   userIds[socket.id] = currentUserId++;
-  socket.on("message", messageText => {
-    const userId = userIds[socket.id];
-    const message = createMessage(userId, messageText);
-    console.log(message);
-    socket.broadcast.emit("message", message);
-  });
+  messageHandler.handleMessage(socket, userIds);
 });
 
 io.listen(3001);
