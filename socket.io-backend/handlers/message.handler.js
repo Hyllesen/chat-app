c7a4da6aeb5db93b93c8ea8c263ab0fed36da972
Hyllesen <stefanhyltoft@gmail.com>
2019-10-16 01:52:14 +0200

    Unique user avatars

--- socket.io-backend/handlers/message.handler.js
+++ socket.io-backend/handlers/message.handler.js
@@ -8,7 +8,7 @@ function createMessage(user, messageText) {
     user: {
       _id: user.userId,
       name: user.username,
-      avatar: "https://placeimg.com/140/140/any"
+      avatar: user.avatar
     }
   };
 }
--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -4,12 +4,19 @@ const messageHandler = require("./handlers/message.handler");
 let currentUserId = 2;
 const users = {};
 
+function createUserAvatarUrl() {
+  const rand1 = Math.round(Math.random() * 200 + 100);
+  const rand2 = Math.round(Math.random() * 200 + 100);
+  return `https://placeimg.com/${rand1}/${rand2}/any`;
+}
+
 io.on("connection", socket => {
   console.log("a user connected!");
   console.log(socket.id);
   users[socket.id] = { userId: currentUserId++ };
   socket.on("join", username => {
     users[socket.id].username = username;
+    users[socket.id].avatar = createUserAvatarUrl();
     messageHandler.handleMessage(socket, users);
   });
 });
