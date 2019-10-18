07017e928a835085fc9db439f26cc0e19e3dd515
Hyllesen <stefanhyltoft@gmail.com>
2019-10-18 04:31:23 +0200

    Added redux boilerplate

--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -19,6 +19,13 @@ io.on("connection", socket => {
     users[socket.id].avatar = createUserAvatarUrl();
     messageHandler.handleMessage(socket, users);
   });
+  socket.on("action", action => {
+    switch (action.type) {
+      case "server/hello":
+        console.log("Got hello event", action.data);
+        socket.emit("action", { type: "message", data: "Good day!" });
+    }
+  });
 });
 
 io.listen(3001);
