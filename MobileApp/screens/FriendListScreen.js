2ab6b97fc9d916754072826e85de089ebf393e10
Hyllesen <stefanhyltoft@gmail.com>
2019-10-19 02:45:11 +0200

    UserAvatar and touchable added to friendlist

--- MobileApp/screens/FriendListScreen.js
+++ MobileApp/screens/FriendListScreen.js
@@ -1,22 +1,49 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
 import { useSelector } from "react-redux";
-import { FlatList } from "react-native-gesture-handler";
 
 export default function FriendListScreen() {
   const usersOnline = useSelector(state => state.usersOnline);
   console.log("usersOnline", usersOnline);
 
+  const { itemContainerStyle, avatarImgStyle, avatarNameViewStyle } = styles;
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <View style={{ flex: 1 }}>
       <FlatList
         data={usersOnline}
         renderItem={({ item }) => {
           console.log("item", item);
-          return <Text>{item.username}</Text>;
+          return (
+            <TouchableOpacity>
+              <View style={itemContainerStyle}>
+                <Image style={avatarImgStyle} source={{ uri: item.avatar }} />
+                <View style={avatarNameViewStyle}>
+                  <Text style={{ fontSize: 20 }}>{item.username}</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          );
         }}
-        keyExtractor={item => item.username}
+        keyExtractor={item => item.userId}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  itemContainerStyle: { flex: 1, flexDirection: "row" },
+  avatarImgStyle: { width: 100, height: 100, borderRadius: 50 },
+  avatarNameViewStyle: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
